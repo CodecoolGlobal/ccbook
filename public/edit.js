1fe589c6ed6b5ee459506a754ba367abd5aee2c2
FlorinCiocirlan <florineless@gmail.com>
2020-09-12 18:05:12 +0300

    added few styling changes , show just completed info on profile page, changed js form verification

--- public/edit.js
+++ public/edit.js
@@ -1,34 +1,45 @@
-let inputs = document.getElementsByTagName('input');
+let formDiv = document.getElementById('formDiv');
+let p = document.createElement('p');
+p.setAttribute('id', 'formFeedback');
+p.setAttribute('style', 'text-align:center;color:red');
+document.getElementById('feedback').appendChild(p);
+
+
 let submitButton = document.getElementById('submitButton');
-submitButton.addEventListener("click",function (e){
+submitButton.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(submitButton);
 })
 
-document.getElementById('btnFile').addEventListener('click',function (){
+document.getElementById('btnFile').addEventListener('click', function () {
     document.getElementById('uploadFile').click();
 })
 
 
 submitButton.addEventListener('click', function () {
+    let inputs = formDiv.getElementsByTagName('input');
     let counter = 0;
+    let firstNameInput = document.querySelector('[name="first_name"]')
+    let lastNameInput = document.querySelector('[name="last_name"]')
+    let birthdateInput = document.querySelector('[name="birthdate"]')
+
     for (let input of inputs) {
-        if (input && input.value) {
+        console.log(input.value);
+        if (input && input.value.length!==0) {
             counter++;
         }
     }
-    if (counter < 5) {
-        if (!document.getElementById('formFeedback')) {
 
-            let p = document.createElement('p');
-            p.setAttribute('id', 'formFeedback');
-            p.setAttribute('style','text-aling:center;color:red');
+    if ([firstNameInput.value,lastNameInput.value,birthdateInput.value].some((input)=>input==0)) {
+        if (p.innerText!=='Please fill in your first name , last name and birthdate') {
+            p.innerText = 'Please fill in your first name , last name and birthdate';
+        }
+    } else if (counter < 5) {
+        if (p.innerText!=='You must have completed at least 5 fields') {
             p.innerText = 'You must have completed at least 5 fields';
-            document.getElementById('feedback').appendChild(p);
         }
     } else {
         let form = document.getElementById('form-edit');
-
         form.submit();
     }
 });
--- public/utilities.php
+++ public/utilities.php
@@ -1,3 +1,14 @@
 <?php
+function sanitizeProfileData($profile){
+    $ignoredData = ['id','user_id','image_id','main_profile','deleted','first_name','last_name','birthdate'];
+    $sanitizedData =[];
+    foreach ($profile as $key => $value){
+        if(!in_array($key,$ignoredData) && $profile[$key] != null){
+                $sanitizedData[ucwords($key)]=ucwords($value);
+        }
+    }
 
+    return $sanitizedData;
+
+}
 

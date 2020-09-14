71161abe538fcd50cd9bed6b95e9e3c66f2e96a0
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-09-14 04:23:03 +0300

    info zone

--- templates/default.php
+++ templates/default.php
@@ -16,12 +16,15 @@
 
     <!-- Custom made -->
     <script rel="script" src="public/statics.js"></script>
+    <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
     <link rel="stylesheet" href="public/frontPageStyle.css">
     <link rel="stylesheet" href="public/footer.css">
     <title>CodeCoolerBook</title>
 
 </head>
-
+<body>
+<div class="pageContainer">
+    <div class="contentWrap">
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <a class="navbar-brand " href="#">
         <img src="public/images/ccbLOGO.png" width="50" height="50" class="d-inline-block align-top" alt="" loading="lazy">
@@ -45,8 +48,133 @@
     </div>
 </nav>
 
-<body>
 
+<section id="showcase">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="showcase-left">
+                    <img src="public/images/1.jpg">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="showcase-right">
+                    <h1>Join Our Comunity</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <br>
+                <a class="btn btn-default btn-lg showcase-btn">Read More</a>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section id="testimonial">
+    <div class="container">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit suscipit, perspiciatis earum non culpa minus labore. Dolore quae saepe aperiam nam iste sapiente minima tenetur necessitatibus, repudiandae recusandae, sequi sed!</p>
+        <p class="customer">- John Doe</p>
+    </div>
+</section>
 
+<section id="info1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="info-left">
+                    <img src="public/images/2.jpg">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="info-right">
+                    <h2>Get To Know Your Mates Better</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore quos praesentium voluptate, voluptas illum quasi aliquid harum possimus earum vel, enim distinctio tenetur excepturi. Aliquid totam assumenda minus quis dicta!</p>
+                    <br>
+                    <a class="btn btn-default btn-lg">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<hr>
+<section id="info2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <div class="info-left">
+                    <h2>Info Block One</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore quos praesentium voluptate, voluptas illum quasi aliquid harum possimus earum vel, enim distinctio tenetur excepturi. Aliquid totam assumenda minus quis dicta!</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="info-right">
+                    <h2>Info Block Two</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore quos praesentium voluptate, voluptas illum quasi aliquid harum possimus earum vel, enim distinctio tenetur excepturi. Aliquid totam assumenda minus quis dicta!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</div>
 <?php require 'layout/footer.php' ?>
+</div>
+</body>
+
+<script>
+    window.sr = ScrollReveal();
+    sr.reveal('.navbar', {
+        duration: 2000,
+        origin:'bottom'
+    });
+    sr.reveal('.showcase-left', {
+        duration: 2000,
+        origin:'top',
+        distance:'300px'
+    });
+    sr.reveal('.showcase-right', {
+        duration: 2000,
+        origin:'right',
+        distance:'300px'
+    });
+    sr.reveal('.showcase-btn', {
+        duration: 2000,
+        delay: 2000,
+        origin:'bottom',
+        distance:'300px'
+    });
+    sr.reveal('#testimonial div', {
+        duration: 2000,
+        origin:'bottom'
+    });
+    sr.reveal('.info-left', {
+        duration: 2000,
+        origin:'left',
+        distance:'300px',
+        viewFactor: 0.2
+    });
+    sr.reveal('.info-right', {
+        duration: 2000,
+        origin:'left',
+        distance:'300px',
+        viewFactor: 0.2
+    });
+</script>
+
+<script>
+    $(function() {
+        //Smooth Scrolling
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if(location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+                location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+</script>
--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,8 +1,8 @@
-<?= '</body>' ?>
-<div class="footer">
+
+<div class="footer2">
     <div class="footer__inner">
         <div class="footer__logo-container">
-            <img src="" alt="logo" />
+            <img src="public/images/ccbLOGO.png" alt="logo" />
         </div>
         <div class="footer_third">
             <h1>Need Help?</h1>
@@ -11,18 +11,20 @@
         </div>
         <div class="footer_third">
             <h1>More</h1>
-            <a href="https://www.imdb.com/"> IMDb</a>
-            <a href="https://www.netflix.com/"> Netflix</a>
-            <a href="https://www.youtube.com/"> YouTube</a>
+            <a href="#"> Link</a>
+            <a href="#"> Link</a>
+            <a href="#"> Link</a>
         </div>
         <div class="footer_third">
-            <h1>Follow Us</h1>
-            <li><a href="https://www.facebook.com/ady.pogar"><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a></li>
-            <li><a href="https://www.linkedin.com/in/stefan-d-837bb1193/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
-            <li><a href="https://github.com/stefandurlesteanu"><FontAwesomeIcon icon={['fab', 'github-alt']} /></a></li>
-            <li><a href="mailto: florineless@gmail.com"><FontAwesomeIcon icon={['fa', 'envelope']} /></a></li>
+            <h1>Follow Us :</h1>
+
+                <li><a href="https://www.facebook.com/ady.pogar" target="_blank"><i class="fa fa-facebook-f"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/stefan-d-837bb1193/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/orgs/CodecoolGlobal/teams/phpmonsters/repositories" target="_blank"><i class="fa fa-github"></i></a></li>
+                <li><a href="mailto: florineless@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a></li>
+
             <span>
-						&copy;{new Date().getFullYear()} TEAM Maniacs | All rights reserved
+						&copy;<script>document.write(currentDate())</script> TEAM Maniacs | All rights reserved
 					</span>
         </div>
 
@@ -38,5 +40,5 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-</body>
-</html>
+
+
